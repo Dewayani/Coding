545621d9b04a9310d64783539d9404c81ffa1e92
Dewayani <73473607+Dewayani@users.noreply.github.com>
2022-07-20 01:22:44 +0530

    updated

--- CodingJava.java
+++ CodingJava.java
@@ -19,4 +19,31 @@ public class CodingJava {
 		String Country = driver.findElement(By.xpath("//div[@class=\"ipc-metadata-list-item__content-container\"]//following::ul[@class=\"ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base\"]//following::li[@class=\"ipc-inline-list__item\"]//following::a[@href=\"/search/title/?country_of_origin=IN&ref_=tt_dt_cn\" and @class=\"ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link\"]")).getText();
 		System.out.println(Releasedate + " is ReleaseDate");
 		System.out.println(Country + " is Country");
+		
+		
+		driver.get("https://en.wikipedia.org/wiki/Pushpa:_The_Rise"); //hit url on browser
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle()); //validate if page title is correct.
+		System.out.println(driver.getCurrentUrl()); //validate if landed on correct url
+		String ReleasedateSecond = driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[12]/td/div/ul/li")).getText();
+		String CountrySecond = driver.findElement(By.xpath("//*[text()=\"Country\"]//following::td[text()=\"India\"]")).getText();
+		System.out.println(ReleasedateSecond + " is ReleasedateSecond");
+		System.out.println(CountrySecond + " is CountrySecond");
+		
+		driver.close();
+		
+		if(Releasedate!=ReleasedateSecond)
+		{
+		Assert.assertNotEquals(Releasedate, ReleasedateSecond);
+		System.out.println("Dates are not same");
+		}
 	
+		if(Country!=CountrySecond)
+		{
+		Assert.assertEquals(Country, CountrySecond);
+		System.out.println("Country is same");
+		}
+	
+	}
+
+}
